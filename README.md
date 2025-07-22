f9b88bf9565470d57731eac23899aef9a994f97f
Tyler Liu <tyler4long@gmail.com>
2025-07-22 12:29:14 -0700

    Frist working version

---
+++ README.md
@@ -0,0 +1,23 @@
+# RingCentral Softphone invite Call Queue Agent to Conference Demo
+
+## User Story
+
+A call agent is behind a call queue. We want to use the softphone SDK to make a
+call to the call queue, once it was answered by the call agent, we would like to
+bring the call agent into a conference.
+
+## Setup
+
+```
+yarn install
+```
+
+Create `.env` file using `.env.sample` as template.
+
+Edit `.env` and specify credentials.
+
+## Run
+
+```
+yarn test
+```
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import RingCentral from "@rc-ex/core";
+import Softphone from "ringcentral-softphone";
+import fs from "fs";
 
 const rc = new RingCentral({
   server: process.env.RINGCENTRAL_SERVER_URL,
@@ -6,11 +8,66 @@ const rc = new RingCentral({
   clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
 });
 
+const softphone = new Softphone({
+  outboundProxy: process.env.SIP_INFO_OUTBOUND_PROXY!,
+  username: process.env.SIP_INFO_USERNAME!,
+  password: process.env.SIP_INFO_PASSWORD!,
+  authorizationId: process.env.SIP_INFO_AUTHORIZATION_ID!,
+  domain: process.env.SIP_INFO_DOMAIN!,
+});
+softphone.enableDebugMode();
+
 const main = async () => {
+  // initialize
+  await softphone.register();
   await rc.authorize({
     jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
   });
-  console.log(rc.token.access_token);
-  await rc.revoke();
+
+  // create conference
+  const r = await rc.restapi().account().telephony().conference().post();
+  const confSession = await softphone.call(r.session!.voiceCallToken!);
+  let header = confSession.sipMessage.headers["p-rc-api-ids"];
+  let match = header.match(/party-id=([^;]+);session-id=([^;]+)/)!;
+  const confSessionId = match[2];
+
+  // record conference audio
+  const writeStream = fs.createWriteStream(`${confSession.callId}.wav`, {
+    flags: "a",
+  });
+  confSession.on("audioPacket", (rtpPacket) => {
+    writeStream.write(rtpPacket.payload);
+  });
+  // either you or the peer hang up
+  confSession.once("disposed", () => {
+    writeStream.close();
+  });
+
+  // call the call queue
+  const inviteSession = await softphone.call(
+    process.env.RINGCENTRAL_CALL_QUEUE_NUMBER!,
+  );
+  header = inviteSession.sipMessage.headers["p-rc-api-ids"];
+  match = header.match(/party-id=([^;]+);session-id=([^;]+)/)!;
+  const partyId = match[1];
+  const sessionId = match[2];
+  inviteSession.once("answered", async () => {
+    // invite the call queue agent to the conference
+    await rc
+      .restapi()
+      .account()
+      .telephony()
+      .sessions(confSessionId)
+      .parties()
+      .bringIn()
+      .post({
+        sessionId,
+        partyId,
+      });
+
+    // optional, we do not need REST API token after this point
+    await rc.revoke();
+  });
 };
+
 main();
