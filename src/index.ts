71e9b409a88e861ded698a558f47709b29bece74
Tyler Liu <tyler4long@gmail.com>
2025-07-22 11:47:37 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import RingCentral from "@rc-ex/core";
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+const main = async () => {
+  await rc.authorize({
+    jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
+  });
+  console.log(rc.token.access_token);
+  await rc.revoke();
+};
+main();
